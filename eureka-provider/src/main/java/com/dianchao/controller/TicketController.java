72c8d8b8067d8ea8030fee79412c19a6c24bf57f
zdc <947389057@qq.com>
2019-11-03 23:09:49 +0800

    整合eureka注册中心使用

--- eureka-provider/src/main/java/com/dianchao/controller/TicketController.java
+++ eureka-provider/src/main/java/com/dianchao/controller/TicketController.java
@@ -4,7 +4,6 @@ import com.dianchao.sevice.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
